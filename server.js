c0514f7be86331c20e123ad026228f47c33c4317
Arhen Santiago <lincoldarhen9900@gmail.com>
2020-08-18 18:11:15 +0800

    created routes

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const moment = require("moment");
-
 const app = express();
 connectDB();
 
@@ -15,7 +13,4 @@ app.use(
 app.use("/api/projects", require("./routes/api/projects"));
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-  console.log(moment(Date.now()).format("MMM D, YYYY hh:mm A"));
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
