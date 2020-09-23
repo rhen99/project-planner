59ffbcc60b93e8740a3db2831b3ea195716f0b88
Arhen Santiago <lincoldarhen9900@gmail.com>
2020-09-23 15:29:36 +0800

    Version 1.0.0

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const connectDB = require("./config/db");
 const app = express();
 connectDB();
@@ -13,6 +14,15 @@ app.use(
 app.use("/api/projects", require("./routes/api/projects"));
 
 app.use("/api/user", require("./routes/api/user"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
